5a6dcc114412fd189b56999b307d6b770b8aa004
PGGV007 <pedrogaldinopggv@gmail.com>
2025-01-29 23:33:11 -0300

    Conserto

---
+++ src/classesBasicas/User.java
@@ -0,0 +1,43 @@
+package Project;
+
+import java.util.Objects;
+
+public class User extends Pessoa{
+    private String idUser;
+    Registro ownedRegistros;
+
+    public User(String cpf, String nome, String idUser, Registro ownedRegistros) {
+        super(cpf, nome);
+        this.idUser = idUser;
+        this.ownedRegistros = ownedRegistros;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public Registro getOwnedRegistros() {
+        return ownedRegistros;
+    }
+
+    public void setOwnedRegistros(Registro ownedRegistros) {
+        this.ownedRegistros = ownedRegistros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(getIdUser(), user.getIdUser()) && Objects.equals(getOwnedRegistros(), user.getOwnedRegistros());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIdUser(), getOwnedRegistros());
+    }
+}
