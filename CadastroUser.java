c97ca4c3052e46b8e67700ba6e8b0d62871a9aaf
André Vinícius <avnc@cin.ufpe.br>
2025-02-24 09:13:17 -0300

    Consertei o método de atualizar usuário

---
+++ CadastroUser.java
@@ -0,0 +1,66 @@
+package business;
+
+import dados.IRepositorio;
+import exceptions.EmptyArchiveException;
+import exceptions.ObjectOutsideArrayException;
+
+import java.io.IOException;
+
+import classesBasicas.User;
+import exceptions.UserAlreadyExistsException;
+import exceptions.UnregisteredUserException;
+
+public class CadastroUser {
+	
+	private IRepositorio repositorio; 
+	
+	public CadastroUser(IRepositorio repositorio) {
+		this.repositorio = repositorio; 
+	}
+	
+	public void cadastrar(User a) throws UserAlreadyExistsException, IOException, ClassNotFoundException, EmptyArchiveException {
+		if(a == null) {
+			throw new IllegalArgumentException("Parâmetro inválido"); 
+		}
+		else {
+			if(this.existe(a) == false) {
+				this.repositorio.adicionar(a);
+			}else {
+				throw new UserAlreadyExistsException();  
+			}
+		}
+	}
+	
+	public boolean existe(User a) throws ClassNotFoundException, IOException, EmptyArchiveException  {
+		return repositorio.existe(a.getIdUser()); 
+	}
+	
+	public void descadastrar(String id) throws ObjectOutsideArrayException, UnregisteredUserException, ClassNotFoundException, IOException, EmptyArchiveException {
+		User u = (User) repositorio.procurar(id); 
+		
+		
+		if(u != null) {
+			if(repositorio.existe(id) ==true) {
+				repositorio.retirar(u);
+			}else {
+				throw new UnregisteredUserException(); 
+			}
+		}else {
+			throw new IllegalArgumentException("Id inválida"); 
+		}
+		
+	}
+	
+	public void atualizar(String idUser, Object user) throws ObjectOutsideArrayException, UnregisteredUserException, ClassNotFoundException, IOException, EmptyArchiveException {
+		if(idUser != null) {
+			if(repositorio.existe(idUser) == true) {
+				repositorio.atualizar(user);
+			}else {
+				throw new UnregisteredUserException(); 
+			}
+		}else {
+			throw new UnregisteredUserException(); 
+		}
+	}
+	
+}
